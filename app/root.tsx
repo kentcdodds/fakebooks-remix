4601bdfc49395e91b2a9ad6328e9f025f058d3cf
Kent C. Dodds <me+github@kentcdodds.com>
2022-04-22 17:04:16 -0600

    fix title

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Fakebooks Remix",
   viewport: "width=device-width,initial-scale=1",
 });
 
