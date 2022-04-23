384b3fac98399562e78563eb68db844da552ad8a
Kent C. Dodds <me+github@kentcdodds.com>
2022-04-22 23:00:12 -0600

    try this

--- app/root.tsx
+++ app/root.tsx
@@ -19,7 +19,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Fakebooks Remix",
-  viewport: "width=device-width,initial-scale=1",
+  // viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
