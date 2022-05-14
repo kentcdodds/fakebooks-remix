db8ab7c3921b84406ee40b472311ba857853e589
Alex S S Fonseca <alexkads@gmail.com>
2022-05-14 10:56:44 -0300

    Simplificando a consulta com funções nativa
    Acredito que assim fique mais performático, porem é sensível os caracteres maiúsculos e minúsculos.

--- app/models/customer.server.ts
+++ app/models/customer.server.ts
@@ -1,18 +1,25 @@
 import { prisma } from "~/db.server";
 
 export async function searchCustomers(query: string) {
-  const customers = await prisma.customer.findMany({
+  return await prisma.customer.findMany({
     select: {
       id: true,
       name: true,
       email: true,
     },
-  });
-  const lowerQuery = query.toLowerCase();
-  return customers.filter((c) => {
-    return (
-      c.name.toLowerCase().includes(lowerQuery) ||
-      c.email.toLowerCase().includes(lowerQuery)
-    );
+    where: {
+      OR: [
+        {
+          email: {
+            equals: query,
+          },
+        },
+        {
+          fullName: {
+            equals: query,
+          },
+        },
+      ],
+    },
   });
 }
