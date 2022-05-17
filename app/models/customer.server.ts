5cff6c63a92007f028fecd92d1fd4457471091c6
Kent C. Dodds <me+github@kentcdodds.com>
2022-05-17 09:04:38 -0600

    insensitive is not supported

--- app/models/customer.server.ts
+++ app/models/customer.server.ts
@@ -3,24 +3,19 @@ import { prisma } from "~/db.server";
 import { getInvoiceDerivedData } from "./invoice.server";
 
 export async function searchCustomers(query: string) {
-  return await prisma.customer.findMany({
+  const customers = await prisma.customer.findMany({
     select: {
       id: true,
       name: true,
       email: true,
     },
-     where: {
-      OR: {
-        email: {
-          contains: query,
-          mode: 'insensitive',
-        },
-        name: {
-          contains: query,
-          mode: 'insensitive',
-        },
-      },
-    },
+  });
+  const lowerQuery = query.toLowerCase();
+  return customers.filter((c) => {
+    return (
+      c.name.toLowerCase().includes(lowerQuery) ||
+      c.email.toLowerCase().includes(lowerQuery)
+    );
   });
 }
 
